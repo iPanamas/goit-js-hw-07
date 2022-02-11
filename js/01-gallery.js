a58eb75844bbcf7a3ab5ba4759e7f3909bc1cdea
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-02-11 17:09:55 +0200

    done homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const createMarkupItems = ({ preview, original, description }) =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+const createGalleryMarkup = galleryItems.map(createMarkupItems).join("");
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+const modalWindow = (src) => {
+  const onEscPress = (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  };
+  const instance = basicLightbox.create(`<img src="${src}"></img>`, {
+    onShow: (instance) => {
+      window.addEventListener("keydown", onEscPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener("keydown", onEscPress);
+    },
+  });
+  instance.show();
+};
+
+galleryRef.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  modalWindow(event.target.dataset.source);
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createMarkupItems = ({ preview, original, description }) =>
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+const createGalleryMarkup = galleryItems.map(createMarkupItems).join("");
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
